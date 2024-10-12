7e26dcbc28d0d95d2ee52f8ef6a638df0fd89b2e
“Viktoriia <victoriia.boiechko@gmail.com>
2024-10-12 13:39:31 +0100

    again

--- User.py
+++ User.py
@@ -1,8 +1,21 @@
 import math
 class User:
+    score = 0
+    lives = 3
     
-    def __init__(self, x_pos, y_pos):
-        self.angle_position = math.atan2(x_pos,y_pos)/math.pi*180
+    def __init__(self, position):
+        self.angle_position = math.atan2(position[0],position[1])/math.pi*180
         self.distance = 1 #for now
         
+    def update_position(self, position):
+        self.angle_position =  math.atan2(position[0],position[1])/math.pi*180
+    
+    def get_position(self):
+        return self.angle_position
+    
+    def reduce_lives(self):
+        self.lives-=1
+    
+    def add_score(self, score):
+        self.score+=score
     
