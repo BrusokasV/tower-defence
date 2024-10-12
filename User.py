894a06a0fce31621d72c2d2875a64ab088ad4742
“Viktoriia <victoriia.boiechko@gmail.com>
2024-10-12 13:52:55 +0100

    added Entity, inheritance in User and Enemies

--- User.py
+++ User.py
@@ -1,17 +1,11 @@
-import math
-class User:
+import math, Entity
+class User(Entity):
     score = 0
     lives = 3
     
     def __init__(self, position):
-        self.angle_position = math.atan2(position[0],position[1])/math.pi*180
-        self.distance = 1 #for now
+        super(Entity, self).__init__(position, 1)
         
-    def update_position(self, position):
-        self.angle_position =  math.atan2(position[0],position[1])/math.pi*180
-    
-    def get_position(self):
-        return self.angle_position
     
     def reduce_lives(self):
         self.lives-=1
