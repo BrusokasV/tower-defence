88863e84fc7052b997066a1f6bbabc6a99a3a0d3
Vladislav Brusokas <brusvlad4002@yandex.ru>
2024-10-12 13:58:46 +0100

    cursor added, main updated

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import pygame
+import User
+from sys import exit
 
 pygame.init()
 
-pygame.display.set_mode((100, 50))
+screen = pygame.display.set_mode((100, 50))
+user = User((0, 0))
+img = pygame.image.load("cursor.png")
 
-running =  True
+running = True
 
 while running:
+    User.update_position(user, pygame.mouse.get_pos())
+
+    screen.fill((255, 255, 255))
+    screen.blit(img, user.get_position())
 
     for event in pygame.event.get():  
         if event.type == pygame.QUIT:  
-           running = False
+           exit()
